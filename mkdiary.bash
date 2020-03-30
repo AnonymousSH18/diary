94e35789eb3f5889e1820c1beb5810c0bc73c2f4
AnonymousSH18 <musiccord2@gmail.com>
2020-03-31 00:42:16 +0900

    origin

---
+++ mkdiary.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+directory="${HOME}/diary" #HOME=/home/tomita
+# If there is no directory, make directory
+if [ ! -d "$directory" ]; then
+    mkdir "$directory"
+fi
+# create file path
+diaryfile="${directory}/$(date '+%Y-%m-%d').txt"
+# insert date
+if [ ! -e "$diaryfile" ]; then
+    date '+%Y/%m/%d' > "$diaryfile"
+fi
+vim "$diaryfile"
+
